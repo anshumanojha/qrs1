0623d0e5f85c3e6eb4127279b511cba9492a2f7b
anshumanojha <55602333+anshumanojha@users.noreply.github.com>
2023-09-05 13:26:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import qrcode
 from PIL import Image
-import io
 
 def generate_qr_code(url):
     if url:
@@ -14,13 +13,11 @@ def generate_qr_code(url):
         qr.add_data(url)
         qr.make(fit=True)
         qr_img = qr.make_image(fill_color="black", back_color="white")
-
-        # Convert the QR code image to bytes
-        qr_byte_io = io.BytesIO()
-        qr_img.save(qr_byte_io, format="PNG")
-        qr_byte_io.seek(0)
-
-        return Image.open(qr_byte_io)
+        
+        # Convert the PIL Image to bytes
+        img_byte_array = io.BytesIO()
+        qr_img.save(img_byte_array, format="PNG")
+        return img_byte_array.getvalue()
 
 def main():
     st.title("QR Code Generator")
@@ -28,8 +25,8 @@ def main():
     user_url = st.text_input("Enter the URL:")
     
     if user_url:
-        qr_img = generate_qr_code(user_url)
-        st.image(qr_img, caption="QR Code", use_column_width=True)
+        qr_img_data = generate_qr_code(user_url)
+        st.image(qr_img_data, caption="QR Code", use_column_width=True, format="PNG")
 
 if __name__ == "__main__":
     main()
