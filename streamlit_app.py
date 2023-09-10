aff8849ba100974d71766eb38e85f636786f3aca
anshumanojha <55602333+anshumanojha@users.noreply.github.com>
2023-09-10 21:20:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,21 @@
 import streamlit as st
 import qrcode
 from PIL import Image, ImageDraw
+import io
 
 def generate_qr_code(url):
     if url:
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=5,  # Decreased box size to make the QR code smaller
-            border=2,    # Decreased border size for better visual appearance
+            box_size=5,
+            border=2,
         )
         qr.add_data(url)
         qr.make(fit=True)
         
         # Create a smaller blank white image to place the QR code on
-        qr_img = Image.new("RGB", (150, 150), "white")  # Adjust dimensions here
+        qr_img = Image.new("RGB", (150, 150), "white")
         draw = ImageDraw.Draw(qr_img)
         
         # Convert the QR code matrix to a Pillow Image
@@ -29,7 +30,19 @@ def main():
     
     if user_url:
         qr_img = generate_qr_code(user_url)
-        st.image(qr_img, caption="QR Code", use_column_width=True)  
+        
+        # Display the QR code image
+        st.image(qr_img, caption="QR Code", use_column_width=True)
+        
+        # Create a download button for the image
+        img_bytes = io.BytesIO()
+        qr_img.save(img_bytes, format="PNG")
+        st.download_button(
+            label="Download QR Code",
+            data=img_bytes.getvalue(),
+            file_name="qr_code.png",
+            mime="image/png",
+        )
 
 if __name__ == "__main__":
     main()
