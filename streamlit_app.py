510268730494f94c872ad714f6dad46818e0882c
anshumanojha <55602333+anshumanojha@users.noreply.github.com>
2023-12-01 15:23:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,25 @@ import requests
 import pandas as pd
 import random
 
+# URL Shortener Function
+def get_smaller_urls(search_query):
+    url = "https://google.com/search?q=" + search_query
+    request_result = requests.get(url)
+    search_results = request_result.text
+    start_index = search_results.find("https://www.zomato.com/")
+    end_index = search_results.find("&", start_index)
+    smaller_url = search_results[start_index:end_index]
+    return smaller_url
+
 # Odd One Out Game Logic
 class OddOneOutGame:
     def __init__(self):
         self.sets = [
-            ['Python', 'JavaScript', 'Java', 'C#'],
-            ['Frontend', 'Backend', 'Database', 'UI/UX'],
-            ['Git', 'Docker', 'AWS', 'Kubernetes']
+            ['Apple', 'Banana', 'Orange', 'Carrot'],
+            ['Dog', 'Cat', 'Fish', 'Bird'],
+            ['Red', 'Blue', 'Green', 'Chair']
         ]
-        self.correct_answers = ['C#', 'UI/UX', 'AWS']
+        self.correct_answers = ['Carrot', 'Fish', 'Chair']
         self.current_set = None
         self.answer = None
 
@@ -21,18 +31,14 @@ class OddOneOutGame:
 
 # Streamlit App
 def main():
-    # Customizing the background color to dark green
-    bg_color = '#006400'  # Dark Green
-    st.markdown(f"""
-        <style>
-            .reportview-container {{
-                background-color: {bg_color};
-                color: #FFFFFF;  /* Text color */
-            }}
-        </style>
-    """, unsafe_allow_html=True)
+    st.title("Anshuman's Portfolio with Odd One Out Game")
 
-    st.title("Andrew's Developer Portfolio with Odd One Out Game")
+    # URL Shortener
+    st.header('URL Shortener')
+    search_query = st.text_input("Enter a search query:")
+    if st.button("Shorten URL"):
+        smaller_url = get_smaller_urls(search_query)
+        st.success(f"Shortened URL: {smaller_url}")
 
     # Odd One Out Game
     game = OddOneOutGame()
@@ -55,29 +61,37 @@ def main():
             st.error("Oops! That's not the odd one out. Try again!")
 
     # Rest of your existing code for the portfolio display
-    st.title("Andrew's Developer Portfolio")
+    st.title("Anshuman's Portfolio")
     st.markdown(
-        "<p style='font-size: 20px;'>Software Developer | Experience: 2+ years</p>",
+        "<p style='font-size: 20px; color: #555555;'>Finops & Revenue Analyst | Experience: 3+ years</p>",
         unsafe_allow_html=True
     )
     # Add links to LinkedIn and GitHub profiles
-    st.markdown("[LinkedIn Profile](https://www.linkedin.com/in/andrew)")
-    st.markdown("[GitHub Profile](https://github.com/andrew)")
+    st.markdown("[LinkedIn Profile](https://www.linkedin.com/in/anshuman-ojha-34093885/)")
+    st.markdown("[GitHub - Python Projects Automated Google Search](https://github.com/anshumanojha/pythonprojects/blob/master/Googlewebsearchauto.ipynb)")
+    st.markdown("[GitHub - Python Projects Automated Location Automate](https://github.com/anshumanojha/pythonprojects/blob/master/browser.py)")
 
     # Create bar chart for tools data
-    tools_data = [9, 8, 7, 9]
-    tools_labels = ['Python', 'JavaScript', 'Git', 'Docker']
+    tools_data = [10, 10, 8, 9, 7]
+    tools_labels = ['MYSQL', 'Python', 'Dashboard Development', 'Power Bi']
     tools_chart = dict(zip(tools_labels, tools_data))
 
     # Create line chart for technology data
-    technology_data = [8, 9, 7, 8, 9]
-    technology_labels = ['React', 'Node.js', 'Spring Boot', 'AWS', 'Databases']
+    technology_data = [9, 10, 10, 8, 7, 10]
+    technology_labels = ['Superset', 'SQL', 'Python', 'AWS', 'AI', 'ML']
     technology_chart = dict(zip(technology_labels, technology_data))
 
-    # Create pie chart for skills data
-    skills_data = [60, 50, 70, 40, 55]
-    skills_labels = ['Web Development', 'API Integration', 'Microservices', 'Cloud Computing', 'Database Management']
-    skills_chart = dict(zip(skills_labels, skills_data))
+    # Example of finding errors in SQL code
+    st.header('Example: Finding Errors in SQL Code')
+    sql_code = st.text_area("Enter your SQL code:")
+    
+    if st.button("Find Errors"):
+        # Replace this part with your actual logic for finding errors in SQL code
+        # For now, let's assume any code containing the word 'error' is incorrect
+        if 'error' in sql_code.lower():
+            st.error("Error found in SQL code. Please review and correct.")
+        else:
+            st.success("No errors found in SQL code. Looks good!")
 
     col1, col2, col3 = st.columns(3)
 
@@ -91,14 +105,60 @@ def main():
 
     with col3:
         st.header('Skills Proficiency')
-        st.bar_chart(skills_chart, use_container_width=True)
+        st.write("Example: Ability to find errors in SQL code")
 
-    st.header('Projects')
-    st.subheader('Project 1: E-commerce Website')
-    st.write("Description: Developed a scalable e-commerce website with payment gateway integration.")
+    st.header('Certifications')
+    st.markdown(
+        "<ul style='font-size: 18px; color: #333333; list-style-type: square;'>"
+        "<li><a href='https://www.coursera.org/account/accomplishments/certificate/PHKLT6LDUU3V' target='_blank'>IBM-Data Analysis certificate</a></li>"
+        "<li><a href='https://www.coursera.org/account/accomplishments/certificate/YWQBBWNA4CHX' target='_blank'>IBM-Data Visualization with Python</a></li>"
+        "<li><a href='https://www.coursera.org/account/accomplishments/certificate/ST57AP42DMXS' target='_blank'>Databases and SQL for Data Science with Python</a></li>"
+        "<li><a href='https://www.coursera.org/account/accomplishments/certificate/PWQGKGYMMMQU' target='_blank'>Machine Learning with Python</a></li>"
+        "<li><a href='https://www.coursera.org/account/accomplishments/certificate/EYQS7XR5JQGV' target='_blank'>Python for Data Science, AI & Development</a></li>"
+        "<li><a href='https://www.coursera.org/account/accomplishments/specialization/certificate/YBQ7NCKANHJ9' target='_blank'>IBM Data Science Specialization</a></li>"
+        "</ul>",
+        unsafe_allow_html=True
+    )
+    # Map section
+    st.header('Location - Bengaluru')
+    # Create a DataFrame with your location data
+    location_df = pd.DataFrame({'LATITUDE': [12.9716], 'LONGITUDE': [77.5946]})
+    st.map(location_df, zoom=10)
 
-    st.subheader('Project 2: Chat Application')
-    st.write("Description: Implemented a real-time chat application using WebSocket technology.")
+    # Project 2: Weather App
+    st.subheader('Project 2: Weather App')
+    st.write("Description: A Python script to fetch weather information for a given city.")
+
+    def get_weather(city):
+        api_key = "4cb3a44e7cab1adb19e17ecc44c6da11"  # Replace with your OpenWeatherMap API key
+
+        base_url = "http://api.openweathermap.org/data/2.5/weather"
+        params = {
+            "q": city,
+            "appid": api_key,
+            "units": "metric"
+        }
+
+        try:
+            response = requests.get(base_url, params=params)
+            data = response.json()
+
+            if data["cod"] == 200:
+                city_name = data["name"]
+                temperature = data["main"]["temp"]
+                weather_desc = data["weather"][0]["description"]
+
+                weather_info = f"City: {city_name}\nTemperature: {temperature}°C\nWeather: {weather_desc}"
+                st.info(weather_info)
+            else:
+                st.error(f"Failed to fetch weather data: {data['message']}")
+
+        except Exception as e:
+            st.error(f"Failed to fetch weather data: {e}")
+
+    city = st.text_input("Enter city name:")
+    if st.button("Get Weather"):
+        get_weather(city)
 
 if __name__ == "__main__":
     main()
