00d4168c43cc776829b26849e90b8f7efe6d517b
anshumanojha <55602333+anshumanojha@users.noreply.github.com>
2023-12-01 12:55:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,9 @@ import streamlit as st
 import qrcode
 from PIL import Image, ImageDraw
 import io
-import webbrowser  # Added for opening WhatsApp link
-import phonenumbers
 
-# Unchanged function
+# Unchanged functions for QR code generation
+
 def generate_qr_code_url(url):
     qr = qrcode.QRCode(
         version=1,
@@ -26,44 +25,12 @@ def generate_qr_code_url(url):
 
     return qr_img
 
-# Unchanged function
-def generate_qr_code_contact_info(contact_info):
-    vcard = f"BEGIN:VCARD\nVERSION:3.0\nFN:{contact_info['Name']}\nEMAIL:{contact_info['Email']}\nTEL:{contact_info['Phone']}\nORG:{contact_info['Current Designation']}\nEND:VCARD"
-
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=5,
-        border=2,
-    )
-    qr.add_data(vcard)
-    qr.make(fit=True)
-
-    qr_size = qr.modules_count
-    min_size = max(150, qr_size * 10)
-
-    qr_img = Image.new("RGB", (min_size, min_size), "white")
-    qr_code_img = qr.make_image(fill_color="black", back_color="white")
-    position = ((min_size - qr_code_img.size[0]) // 2, (min_size - qr_code_img.size[1]) // 2)
-    qr_img.paste(qr_code_img, position)
-
-    return qr_img
-
-# Unchanged function
-def generate_whatsapp_link(phone_number, message):
-    try:
-        parsed_number = phonenumbers.parse(phone_number, "US")  # Change the region code as needed
-        if phonenumbers.is_valid_number(parsed_number):
-            phone_number = phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.E164)
-            return f"https://wa.me/{phone_number}?text={message}"
-    except phonenumbers.NumberFormatException:
-        pass
-    return None
+def generate_experience_summary(company, role, summary):
+    experience_summary = f"Company: {company}\nRole: {role}\nSummary: {summary}"
+    return experience_summary
 
 def main():
-    st.title("QR Code Generator")
-
-    # ... (unchanged)
+    st.title("Andrew's Portfolio")
 
     st.header("Generate QR Code for a Desired URL")
     user_url = st.text_input("Enter the URL:")
@@ -80,40 +47,23 @@ def main():
             mime="image/png",
         )
 
-    st.header("Generate QR Code for Contact Information")
-    name = st.text_input("Name:")
-    email = st.text_input("Email:")
-    phone = st.text_input("Phone:")
-    designation = st.text_input("Current Designation:")
+    st.header("Experience Summary")
+    company = st.text_input("Company:")
+    role = st.text_input("Role:")
+    summary = st.text_area("Summary:")
+    
+    if st.button("Generate Experience Summary") and (company or role or summary):
+        experience_summary = generate_experience_summary(company, role, summary)
+        st.text(experience_summary)
 
-    if st.button("Generate QR Code (Contact Info)") and (name or email or phone or designation):
-        contact_info = {
-            "Name": name,
-            "Email": email,
-            "Phone": phone,
-            "Current Designation": designation,
-        }
-        qr_img_contact_info = generate_qr_code_contact_info(contact_info)
-        st.image(qr_img_contact_info, caption="QR Code with Contact Info", use_column_width=False)
-        img_bytes_contact_info = io.BytesIO()
-        qr_img_contact_info.save(img_bytes_contact_info, format="PNG")
-        st.download_button(
-            label="Download QR Code (Contact Info)",
-            data=img_bytes_contact_info.getvalue(),
-            file_name="qr_code_contact_info.png",
-            mime="image/png",
-        )
+    st.header("Skills and Projects")
+    skills = st.text_area("Skills:")
+    projects = st.text_area("Related Projects:")
 
-    st.header("Send WhatsApp Message")
-    whatsapp_phone_number = st.text_input("Enter WhatsApp Phone Number (with country code, e.g., +123456789):")
-    whatsapp_message = st.text_input("Enter WhatsApp Message:")
-
-    if st.button("Generate WhatsApp Message") and (whatsapp_phone_number and whatsapp_message):
-        whatsapp_link = generate_whatsapp_link(whatsapp_phone_number, whatsapp_message)
-        if whatsapp_link:
-            st.markdown(f"Click [here]({whatsapp_link}) to open WhatsApp and send the message.")
-        else:
-            st.warning("Invalid phone number. Please enter a valid WhatsApp phone number.")
+    if st.button("Save Skills and Projects") and (skills or projects):
+        # You can save or display the skills and projects as needed
+        st.text(f"Skills: {skills}")
+        st.text(f"Related Projects: {projects}")
 
 if __name__ == "__main__":
     main()
